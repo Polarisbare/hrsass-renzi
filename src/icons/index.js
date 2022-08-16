1ec65ff771a6afd9a228fe877f6ea0113353a62b
lvjingxin <1768538073@qq.com>
2022-08-16 17:50:22 +0800

    引入样式和图片

--- src/icons/index.js
+++ src/icons/index.js
@@ -4,6 +4,10 @@ import SvgIcon from '@/components/SvgIcon'// svg component
 // register globally
 Vue.component('svg-icon', SvgIcon)
 
+// require.context 有三个参数
+// 1.要查找的路径、
+// 2.是否查找子目录
+// 3.表示查找那些文件  正则表达式   /\.js/
 const req = require.context('./svg', false, /\.svg$/)
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(req)
