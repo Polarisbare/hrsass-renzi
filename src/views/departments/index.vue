49738c0e7b6c4d0cc8f9b43fb2b18ce5615550e1
lvjingxin <1768538073@qq.com>
2022-08-22 18:04:02 +0800

    作用域插槽完成布局

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -3,67 +3,24 @@
     <div class="app-container">
       <el-card class="tree-card">
         <!-- 用了一个行列布局 -->
-        <el-row type="flex" justify="space-between" align="middle" style="height: 40px">
-          <el-col :span="20">
-            <span>江苏传智播客教育科技股份有限公司</span>
-          </el-col>
-          <el-col :span="4">
-            <el-row type="flex">
-              <!-- 两个内容 -->
-              <el-col :span="12">负责人</el-col>
-              <el-col :span="12">
-                <!-- 下拉菜单 element -->
-                <el-dropdown>
-                  <span>
-                    操作<i class="el-icon-arrow-down" />
-                  </span>
-                  <!-- 下拉菜单 -->
-                  <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>添加子部门</el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-            </el-row>
-          </el-col>
-        </el-row>
+        <trre-tools :node-data="company" />
         <!-- 当el-row 放到其他标签中，宽度是通过内容撑开的 -->
         <el-tree :data="departs" :props="defaultProps">
-          <template #default="{ data }">
-            <el-row type="flex" justify="space-between" align="middle" style="height: 40px;width:100%">
-              <el-col :span="20">
-                <span>{{ data.name }}</span>
-              </el-col>
-              <el-col :span="4">
-                <el-row type="flex">
-                  <!-- 两个内容 -->
-                  <el-col :span="12">{{ data.manager }}</el-col>
-                  <el-col :span="12">
-                    <!-- 下拉菜单 element -->
-                    <el-dropdown>
-                      <span>
-                        操作<i class="el-icon-arrow-down" />
-                      </span>
-                      <!-- 下拉菜单 -->
-                      <el-dropdown-menu slot="dropdown">
-                        <el-dropdown-item>删除部门</el-dropdown-item>
-                        <el-dropdown-item>修改部门</el-dropdown-item>
-                        <el-dropdown-item>添加子部门</el-dropdown-item>
-                      </el-dropdown-menu>
-                    </el-dropdown>
-                  </el-col>
-                </el-row>
-              </el-col>
-            </el-row>
+          <template #default="{data}">
+            <trre-tools :node-data="data" />
           </template>
         </el-tree>
       </el-card>
     </div>
   </div>
 </template>
-
 <script>
+import trreTools from './components/tree-tools.vue'
 export default {
   name: 'Departments',
+  components: {
+    trreTools
+  },
   data() {
     return {
       departs: [
@@ -75,6 +32,7 @@ export default {
         { name: '行政部', manager: '刘备' },
         { name: '人事部', manager: '孙权' }
       ],
+      company: { name: '江苏传智播客教育科技股份有限公司', manager: '负责人' },
       defaultProps: {
         label: 'name'
       }
