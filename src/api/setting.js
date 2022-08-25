aadd0c2e8c20ad6d1b555227c9d14792ad9268fe
lvjingxin <1768538073@qq.com>
2022-08-25 16:48:12 +0800

    完成分页

--- src/api/setting.js
+++ src/api/setting.js
@@ -1,5 +1,10 @@
 import request from '@/utils/request'
-
+/**
+ * 获取所有角色列表
+ * @param {*} page 页数
+ * @param {*} pagesize 页容量
+ * @returns
+ */
 export const getRoleListApi = (page, pagesize) => {
   return request({
     url: '/sys/role',
--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -12,16 +12,29 @@
               type="primary"
             >新增角色</el-button>
             <!-- 表格 -->
-            <el-table>
-              <el-table-column label="序号" width="100" />
-              <el-table-column label="角色名称" width="240" />
-              <el-table-column label="描述" />
+            <el-table :data="list">
+              <el-table-column label="序号" width="100" type="index" />
+              <el-table-column label="角色名称" width="240" prop="name" />
+              <el-table-column label="描述" prop="description" />
               <el-table-column label="操作">
                 <el-button size="small" type="success">分配权限</el-button>
                 <el-button size="small" type="primary">编辑</el-button>
                 <el-button size="small" type="danger">删除</el-button>
               </el-table-column>
             </el-table>
+
+            <!-- 分页 -->
+            <div class="fy">
+              <el-pagination
+                :current-page="page"
+                :page-sizes="[1, 2, 3, 4, 5]"
+                :page-size="pagesize"
+                layout="total, sizes, prev, pager, next, jumper"
+                :total="total"
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+              />
+            </div>
           </el-tab-pane>
 
           <el-tab-pane label="公司信息">
@@ -39,7 +52,10 @@ export default {
   name: 'Setting',
   data() {
     return {
-
+      list: [],
+      total: 0, // 总条数
+      pagesize: 4, // 每页条数
+      page: 1 // 当前页
     }
   },
   created() {
@@ -47,13 +63,34 @@ export default {
   },
   methods: {
     async getRoleList() {
-      const res = await getRoleListApi()
-      console.log(res)
+      const { data } = await getRoleListApi(this.page, this.pagesize)
+      // console.log(data)
+      const { rows, total } = data
+      this.list = rows
+      this.total = total
+    },
+    handleSizeChange(val) {
+      // console.log(`每页 ${val} 条`)
+      this.pagesize = val
+      this.page = 1
+      // 调用方法, 重新请求
+      this.getRoleList()
+    },
+    handleCurrentChange(val) {
+      // console.log(`当前页: ${val}`)
+      this.page = val
+      // 调用方法, 重新请求
+      this.getRoleList()
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss" sacped>
+  .fy{
+    padding: 30px;
+    display: flex;
+    justify-content: flex-end;
 
+  }
 </style>
