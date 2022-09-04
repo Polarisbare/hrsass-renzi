098e37969491a4e95ad05a694c218f37f39c6d1e
lvjingxin <1768538073@qq.com>
2022-09-04 08:23:46 +0800

    完成雷达图

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -84,7 +84,11 @@
           <div slot="header" class="header">
             <span>绩效指数</span>
           </div>
-        <!-- 放置雷达图 -->
+          <!-- 放置雷达图 -->
+          <template>
+            <!-- 雷达图  图表必须给高和宽度-->
+            <div ref="myDiv" class="radar-echart" />
+          </template>
         </el-card>
         <!-- 帮助连接 -->
         <el-card class="box-card">
@@ -123,6 +127,17 @@
 import { mapGetters, mapState } from 'vuex'
 import defaultImg from '@/assets/common/bigUserHeader.png'
 import WorkCalendar from './components/work-calendar.vue'
+// 按需加载
+import * as echarts from 'echarts/core' // 引入主模块
+import { RadarChart } from 'echarts/charts' // 引入雷达图
+// 引入提示框和标题组件
+import { TitleComponent, TooltipComponent, LegendComponent } from 'echarts/components'
+// 引入canvas渲染器
+import { CanvasRenderer } from 'echarts/renderers'
+// 注册必须的组件
+echarts.use(
+  [TitleComponent, TooltipComponent, LegendComponent, RadarChart, CanvasRenderer]
+)
 export default {
   name: 'Dashboard',
   components: {
@@ -136,6 +151,52 @@ export default {
   computed: {
     ...mapGetters(['name']),
     ...mapState('user', ['userInfo'])
+  },
+  mounted() {
+    const myChart = echarts.init(this.$refs.myDiv) // 得到图表实例
+    myChart.setOption({
+      title: {
+        text: '基础雷达图'
+      },
+      tooltip: {},
+      legend: {
+        data: ['平均水平', '个人价值']
+      },
+      radar: {
+        // shape: 'circle',
+        name: {
+          textStyle: {
+            color: '#fff',
+            backgroundColor: '#999',
+            borderRadius: 3,
+            padding: [3, 5]
+          }
+        },
+        indicator: [
+          { name: '工作效率', max: 100 },
+          { name: '考勤', max: 100 },
+          { name: '积极性', max: 100 },
+          { name: '帮助同事', max: 100 },
+          { name: '自主学习', max: 100 },
+          { name: '正确率', max: 100 }
+        ]
+      },
+      series: [{
+        name: '平均 vs 个人',
+        type: 'radar',
+        // areaStyle: {normal: {}},
+        data: [
+          {
+            value: [80, 80, 80, 80, 80, 80],
+            name: '平均水平'
+          },
+          {
+            value: [90, 75, 95, 90, 95, 90],
+            name: '个人价值'
+          }
+        ]
+      }]
+    })
   }
 }
 </script>
@@ -205,6 +266,11 @@ export default {
     top: 15px;
     z-index: 1;
   }
+}
+.radar-echart {
+    width: 500px;
+    height: 400px;
+
 }
 
 .advContent {
